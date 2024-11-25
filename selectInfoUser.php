f227e8c8325c944dcd777b52f1f8f06681b206f2
Jccontrerascruz <jcontre76203@universidadean.edu.co>
2024-11-25 16:52:36 -0500

    actualicion moduli cliente

---
+++ selectInfoUser.php
@@ -0,0 +1,17 @@
+<?php
+
+include "conexionBD.php";
+
+$sql = "SELECT Correo_Electronico, Contraseña, FROM usuarios";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "Correo electronico: " . $row["Correo_Electronico"]. "Contraseña: " . $row["Contraseña"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
